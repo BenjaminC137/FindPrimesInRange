b2c52c5df26d7c6202e1eab32bef0c86b4f2c0f2
benjaminc137 <benjamin.salasek@icloud.com>
2018-08-13 13:41:54 -0500

    mostly done

--- FindPrimesInRange/Program.cs
+++ FindPrimesInRange/Program.cs
@@ -10,14 +10,16 @@ namespace FindPrimesInRange
         static void Main(string[] args)
         {
             //IEnumerable<int> answer = FindPrimesInRange(2, 100);
-            
-            Console.WriteLine(FindPrimesInRange(2, 100));
+            foreach(var number in FindPrimesInRange(2, 100)){
+                Console.WriteLine(number);
+            }
         }
+
         public static IEnumerable<int> FindPrimesInRange(int a, int b){
 
             List<int> data = new List<int>();
 
-        for (int i = 2; i < b; i++)
+        for (int i = a; i <= b; i++)
             {
                 if(IsPrime(i)){
                     
@@ -26,11 +28,12 @@ namespace FindPrimesInRange
             }      
             return data;
         }
-        public static bool IsPrime(int a){
-            
-            for (int i = 2; i < a; i++)
+
+        public static bool IsPrime(int y){
+            int i;
+            for (i = 2; i <= (int)Math.Sqrt(y); i++)
             {
-                if (a % i == 0)
+                if (y % i == 0)
                 {
                     return false;
                 }
