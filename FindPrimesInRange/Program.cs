864492c94f621cf4682a3737fa969c7aa6950111
benjaminc137 <benjamin.salasek@icloud.com>
2018-08-13 09:32:42 -0500

    initial

---
+++ FindPrimesInRange/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FindPrimesInRange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
