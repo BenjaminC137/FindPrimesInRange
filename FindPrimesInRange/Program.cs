cb63bf69d5792c4fb601971d9291e03b323ebf60
benjaminc137 <benjamin.salasek@icloud.com>
2018-08-13 10:36:55 -0500

    halfway there

--- FindPrimesInRange/Program.cs
+++ FindPrimesInRange/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
 
 namespace FindPrimesInRange
 {
@@ -6,7 +9,33 @@ namespace FindPrimesInRange
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //IEnumerable<int> answer = FindPrimesInRange(2, 100);
+            
+            Console.WriteLine(FindPrimesInRange(2, 100));
         }
+        public static IEnumerable<int> FindPrimesInRange(int a, int b){
+
+            List<int> data = new List<int>();
+
+        for (int i = 2; i < b; i++)
+            {
+                if(IsPrime(i)){
+                    
+                    data.Add(i);
+                }
+            }      
+            return data;
+        }
+        public static bool IsPrime(int a){
+            
+            for (int i = 2; i < a; i++)
+            {
+                if (a % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+            }
     }
 }
